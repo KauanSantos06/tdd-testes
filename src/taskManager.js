cd76faa34c313198cc5e14ff109a3ed41d9bba96
KauanSantos06 <kauanuruana@gmail.com>
2026-04-02 09:39:29 -0300

    green: Valida função de remoção

--- src/taskManager.js
+++ src/taskManager.js
@@ -62,3 +62,12 @@ export function toggleTask(task) {
   };
 }
 
+
+// ------------------------------------------------------------
+// Remoção
+// ------------------------------------------------------------
+
+export function removeTask(tasks, taskId) {
+  return tasks.filter((task) => task.id !== taskId);
+}
+
